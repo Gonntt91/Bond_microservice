4a09599e0fe9eb85753bfe31045d7106884453a9
Gon Ton <gon@itz.asia>
2025-03-27 13:34:38 +0700

    account db define

--- libs/common/src/models/company.schema.ts
+++ libs/common/src/models/company.schema.ts
@@ -1,16 +1,26 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
+export enum PRIVILEGE {
+  TIME_CLOCK = "TimeClock",
+  BOND = "Bond",
+  PAYROLL = "Payroll"
+}
+
+
 @Schema()
-export class Company extends Document {
+export class CompanyDocument extends Document {
   @Prop({ required: true, unique: true })
   companyCode: string;
 
   @Prop({ required: true })
   companyName: string;
 
+  @Prop({type: [String], enum: Object.values(PRIVILEGE), default:[]})
+  privileges: string[];
+
   @Prop()
   address: string;
 }
 
-export const CompanySchema = SchemaFactory.createForClass(Company);
+export const CompanySchema = SchemaFactory.createForClass(CompanyDocument);
--- libs/common/src/models/user-company.schema.ts
+++ libs/common/src/models/user-company.schema.ts
@@ -1,18 +1,24 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Types } from 'mongoose';
-import { User } from './user.schema'; // Assuming User is here
-import { Company } from './company.schema';
+import { UserDocument } from './user.schema'; // Assuming User is here
+import { CompanyDocument } from './company.schema';
+
+export enum Role {
+  SUPER_MANAGER = 'super_manager',
+  MANAGER = 'manager',
+  EMPLOYEE = 'employee',
+}
 
 @Schema()
-export class UserCompany extends Document {
+export class UserCompanyDocument extends Document {
   @Prop({ type: Types.ObjectId, ref: 'User', required: true })
   userId: Types.ObjectId;
 
   @Prop({ type: Types.ObjectId, ref: 'Company', required: true })
   companyId: Types.ObjectId;
 
-  @Prop()
-  roles?: string[];
+  @Prop({type: [String], enum: Object.values(Role), default: []})
+  roles: string[];
 }
 
-export const UserCompanySchema = SchemaFactory.createForClass(UserCompany);
+export const UserCompanySchema = SchemaFactory.createForClass(UserCompanyDocument);
